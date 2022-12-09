6299c5bbec89f6f73dd75097d7a0d7ee664308d9
Paschal <offiaelipaschal5@gmail.com>
2022-12-09 02:36:12 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include <stdio.h>                                                                                                                      
-/**                                                                                                                                     
- *  * main - Entry                                                                                                                         
- *   * Return: Always 0                                                                                                                     
- *    */                                                                                                                                     
-int main(void)                                                                                                                          
-{                                                                                                                                       
-        printf("with proper grammar, but the outcome is a piece of art,\n");                                                            
-        return (0);)
-}}
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzl\n");
+	return (0);
+}
