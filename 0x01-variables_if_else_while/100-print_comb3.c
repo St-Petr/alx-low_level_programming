a56155ae1b28cbcfb7717363c0992f78c53adb8e
Paschal <offiaelipaschal5@gmail.com>
2022-12-09 22:33:57 -0800

    print different combinations

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,26 +5,21 @@
  *  Return: Always 0 (success)
  *
  */
-
 int main(void)
 {
 	int ones = '0';
 	int tens = '0';
-
-	for (tens = '0'; tens <= '9'; tens++)/* prints tens digit*/
+	for (tens = '0'; tens <= '9'; tens++)/* prints tens di        git*/
 	{
-		for (ones = '0'; ones <= '9'; ones++)/* printsones digit*/
-		{
-			if (!((ones == tens) || (tens > ones)))/*eliminates repitition*/
-			{
-				putchar(',');
-				putchar(' ');
-			}
-		}
+	for (ones = '0'; ones <= '9'; ones++)/* printsones dig        it*/
+	{
+	if (!((ones == tens) || (tens > ones)))/*eliminates re        pitition*/
+	{
+		putchar(',');
+		putchar(' ');
 	}
-}
-putchar('\n');
-
-return (0);
-
+	}
+	}
+	putchar('\n');
+	return (0);
 }
