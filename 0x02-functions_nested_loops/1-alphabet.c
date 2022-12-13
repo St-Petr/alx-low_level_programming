2d90e55c863528c83f05f3c29ee80667f595e623
Paschal <offiaelipaschal5@gmail.com>
2022-12-13 06:22:31 -0800

    mod

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,7 @@
 #include "main.h"
-#include <unistd.h>
 
 /**
  *print_alphabet - prints the alphabets in lowercase
- *Return: Always 0
  */
 void print_alphabet(void)
 {
@@ -12,6 +10,4 @@ void print_alphabet(void)
 		_putchar(lca);
 	
 	_putchar('\n');
-
-	return (0);
 }
