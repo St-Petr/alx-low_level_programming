40ab4a75d316a64895f78b356e2c5cb17bee7143
Paschal <offiaelipaschal5@gmail.com>
2022-12-13 06:55:29 -0800

    lower character chase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lower case characters
+ * @w: the character
+ * Return: 1 if the letter is lowercase, 0 if not
+ */
+
+int _islower(int w)
+{
+	if (w >= 'a'&& c <= 'z')
+		return (1);
+	else
+		return (0);
+}
