c64378292c073d5608fd322218968d2e34bac176
Paschal <offiaelipaschal5@gmail.com>
2022-12-13 06:01:14 -0800

    print _putchar\n

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,8 +2,8 @@
 #include <unistd.h>
 
 /**
- * main - Entry point 
- * Return : Always 0
+ * main - Entry point
+ * Return: Always 0
  */
 
 int main(void)
@@ -18,5 +18,5 @@ int main(void)
 	_putchar('r');
 	_putchar('\n');
 
-	return(0);
+	return (0);
 }
