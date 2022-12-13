3de22c7b620489890557ef3001c7773c3e456c1f
Paschal <offiaelipaschal5@gmail.com>
2022-12-13 06:35:52 -0800

    print lca in ten lines

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - prints alphabets ten times
+ */
+void print_alphabet_x10.c(void)
+{
+	int ten;
+	char lca;
+
+	for(ten = 0;ten <= 9; ten++)
+	{
+		for(lca = 'a';lca <= 'z'; lca++)
+			_putchar(la);
+		_putchar('\n');
+	}
+}
