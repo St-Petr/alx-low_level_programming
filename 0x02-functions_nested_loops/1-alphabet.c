ab317b21b4ae4b9e704fe36a7baa3aa7f4e86def
Paschal <offiaelipaschal5@gmail.com>
2022-12-13 06:16:43 -0800

    print lowercase alpha to z

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,17 @@
+#include "main.h"
+#include <unistd.h>
 
+/**
+ *print_alphabet - prints the alphabets in lowercase
+ *Return: Always 0
+ */
+void print_alphabet(void)
+{
+	char lca;
+	for(lca = 'a'; lca <= 'z'; lca++)
+		_putchar(lca);
+	
+	_putchar('\n');
+
+	return (0);
+}
